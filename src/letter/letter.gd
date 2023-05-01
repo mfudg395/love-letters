e2e5ee657b4f80037c5080d9834761a78c95a9f3
mfudg395 <mfudg395@mtroyal.ca>
2023-04-30 18:30:04 -0600

    Implemented floating letter animation

--- src/letter/letter.gd
+++ src/letter/letter.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 
+@onready var animation_player = get_node("AnimationPlayer")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,7 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	animation_player.play("idle")
 
 
 func _on_body_entered(_body):
